10e41b2d3b624194d9e8532f80a0d0756d2e101b
Akkibi <88116996+Akkibi@users.noreply.github.com>
2023-07-29 16:58:43 +0200

    set css base for project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar: FunctionComponent = () => {
   return (
     <div className="w-full bg-black p-2 text-white">
       <h1 className="text-11xl">NAV ELEMENTS</h1>
-      <Link to="/">Contact</Link>
+      <Link to="/">Home</Link>
       <br />
-      <Link to="/About">Contact</Link>
+      <Link to="/About">About</Link>
       <br />
       <Link to="/Contact">Contact</Link>
     </div>
